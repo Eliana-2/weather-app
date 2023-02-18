9f4759d6e90687b22089abf0831137299f4c5725
Eliana-2 <eliana.guizar1@gmail.com>
2023-02-18 00:51:37 -0800

    Polished form appearance

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //f3fbe20627eff09cace90cdb9fbe9ee1
 import './style.css';
-import {getWeatherData} from '/modules/weatherData.js'
+import {getWeatherData} from '/modules/weatherData.js';
 import {populateSelect} from './modules/display';
 
 console.log(await getWeatherData('Camarillo', 'United States of America', 'California'));
