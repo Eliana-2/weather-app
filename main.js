83b810163362b29dcc6439df7ec7197c2e98a1be
Eliana-2 <eliana.guizar1@gmail.com>
2023-02-17 21:10:25 -0800

    Add location form & function to show form options

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //f3fbe20627eff09cace90cdb9fbe9ee1
 import './style.css';
 import {getWeatherData} from '/modules/weatherData.js'
+import {populateSelect} from './modules/display';
 
 console.log(await getWeatherData('Camarillo', 'United States of America', 'California'));
-
+populateSelect('country');
+populateSelect('state');
 //switch to 5 day forecast
