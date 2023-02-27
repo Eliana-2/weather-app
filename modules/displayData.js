1b4ae8a9e9e867d2bf4616e07920e48bf35e75dd
Eliana-2 <eliana.guizar1@gmail.com>
2023-02-27 08:37:58 -0800

    Attempt to change display

--- modules/displayData.js
+++ modules/displayData.js
@@ -3,13 +3,25 @@
 function createPanel(dayWeatherData) {
   const displayContainer = document.getElementById('container');
   const panel = document.createElement('div');
+  panel.classList.add('weather-panel');
+  
   const date = document.createElement('h3');
   date.textContent = dayWeatherData.date;
-  const img = document.createElement('img');
-  img.src = `https://openweathermap.org/img/wn/${dayWeatherData.icon}@2x.png`
-
   panel.appendChild(date);
+  
+  const img = document.createElement('img');
+  img.src = `https://openweathermap.org/img/wn/${dayWeatherData.icon}@2x.png`;
+  img.classList.add('panel-icon');
   panel.appendChild(img);
+
+  const temp = document.createElement('p');
+  temp.innerHTML = (dayWeatherData.temp !== undefined) ? `${dayWeatherData.temp} &#8451;` : `${dayWeatherData.minTemp} &#8451; / ${dayWeatherData.maxTemp} &#8451;`;
+  panel.appendChild(temp);
+
+  const weather = document.createElement('p');
+  weather.textContent = dayWeatherData.weather;
+  panel.appendChild(weather);
+
   displayContainer.appendChild(panel);
 }
 
